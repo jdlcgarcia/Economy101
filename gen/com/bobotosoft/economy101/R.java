fe05acd39e9c0edbc30454f19e3d4a104972de99
Juan de la Cruz García García <jcruzgar@gmail.com>
2014-01-27 01:39:02 +0100

    Version to upload to the course

--- gen/com/bobotosoft/economy101/R.java
+++ gen/com/bobotosoft/economy101/R.java
@@ -25,15 +25,11 @@ public final class R {
     }
     public static final class id {
         public static final int LVincome=0x7f080000;
-        public static final int TVamount=0x7f080008;
-        public static final int TVdescription=0x7f080007;
-        public static final int TVperc=0x7f080006;
-        public static final int TVperc_message=0x7f080005;
-        public static final int action_settings=0x7f080009;
-        public static final int bCancelExpense=0x7f080004;
-        public static final int bCancelIncome=0x7f080002;
-        public static final int bNewExpense=0x7f080003;
-        public static final int bNewIncome=0x7f080001;
+        public static final int TVamount=0x7f080004;
+        public static final int TVdescription=0x7f080003;
+        public static final int TVperc=0x7f080002;
+        public static final int TVperc_message=0x7f080001;
+        public static final int action_settings=0x7f080005;
     }
     public static final class layout {
         public static final int activity_income=0x7f030000;
@@ -51,15 +47,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int bCancelExpense=0x7f050007;
-        public static final int bCancelIncome=0x7f050009;
-        public static final int bNewExpense=0x7f050006;
-        public static final int bNewIncome=0x7f050008;
-        public static final int hello_world=0x7f050002;
-        public static final int perc_message=0x7f05000a;
-        public static final int title_activity_expenses=0x7f050003;
-        public static final int title_activity_income=0x7f050004;
-        public static final int title_activity_stats=0x7f050005;
+        public static final int bCancelExpense=0x7f050006;
+        public static final int bCancelIncome=0x7f050008;
+        public static final int bNewExpense=0x7f050005;
+        public static final int bNewIncome=0x7f050007;
+        public static final int perc_message=0x7f050009;
+        public static final int title_activity_expenses=0x7f050002;
+        public static final int title_activity_income=0x7f050003;
+        public static final int title_activity_stats=0x7f050004;
     }
     public static final class style {
         /** 
